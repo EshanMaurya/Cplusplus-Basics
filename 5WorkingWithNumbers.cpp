1cd9b3c4501b09cffe1432888081029dfb31c0c3
EshanMaurya <eshanmaurya0804@gmail.com>
2022-05-13 16:16:23 +0530

    -

--- 5WorkingWithNumbers.cpp
+++ 5WorkingWithNumbers.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    //cout << 10 % 3; // Modulus
+    cout << 10 % 3<<endl; // Modulus
     int intnum = 5;
     double doublenum = 5.5;
 
     intnum++; //Increment
     doublenum --;//decrement
-
-    cout<<10/3; // For integer division, i.e remainder
+    
+    cout<<10/3; // For integer division, i.e returning remainder
 
 }
